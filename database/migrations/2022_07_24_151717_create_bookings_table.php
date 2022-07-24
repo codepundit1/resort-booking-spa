33e11ddce1c2502aa63b976c9a05204e4449304b
codepundit1 <jahid35-266@diu.edu.bd>
2022-07-24 21:29:31 +0600

    3nd commit

--- database/migrations/2022_07_24_151717_create_bookings_table.php
+++ database/migrations/2022_07_24_151717_create_bookings_table.php
@@ -15,6 +15,14 @@ class CreateBookingsTable extends Migration
     {
         Schema::create('bookings', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('resort_id')->nullable()->constrained()->onDelete("cascade");
+            $table->string('user_id')->nullable();
+            $table->string('fullname', 50);
+            $table->string('email')->unique();
+            $table->string('date');
+            $table->string('phone');
+            $table->string('message');
+            $table->string('status')->nullable();
             $table->timestamps();
         });
     }
